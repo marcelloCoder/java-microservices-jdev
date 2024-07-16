f0c77cf312bc71072c82ba1b10161445c11d6fb6
marcelloCoder <deoliveiral767@gmail.com>
2024-07-16 10:38:04 -0300

    Notificacao project microservice created

---
+++ notificacao/src/main/java/br/com/mcoder/pedidos/notificacao/NotificacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.mcoder.pedidos.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoApplication.class, args);
+	}
+
+}
