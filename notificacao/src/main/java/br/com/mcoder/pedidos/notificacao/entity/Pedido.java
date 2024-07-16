afed28aed70cddf2dd4bc12b7592be3293db95dc
marcelloCoder <deoliveiral767@gmail.com>
2024-07-16 14:50:52 -0300

    Notificacao project 4.2  entity created

---
+++ notificacao/src/main/java/br/com/mcoder/pedidos/notificacao/entity/Pedido.java
@@ -0,0 +1,28 @@
+package br.com.mcoder.pedidos.notificacao.entity;
+
+
+import br.com.mcoder.pedidos.notificacao.entity.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pedido {
+    private UUID id = UUID.randomUUID();
+    private String cliente;
+    private List<ItemPedido> itens = new ArrayList<>();
+    private Double valorTotal;
+    private String emailNotificacao;
+    private Status status = Status.EM_PROCESSAMENTO;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dataHora = LocalDateTime.now();
+
+}
---
+++ notificacao/src/main/java/br/com/mcoder/pedidos/notificacao/entity/enums/Status.java
@@ -0,0 +1,5 @@
+package br.com.mcoder.pedidos.notificacao.entity.enums;
+
+public enum Status {
+    EM_PROCESSAMENTO, PROCESSADO;
+}
