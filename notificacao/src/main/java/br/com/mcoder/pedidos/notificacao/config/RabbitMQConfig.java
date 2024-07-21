c7a62ccf8e574b52b27ef396ecab491190059d23
marcelloCoder <deoliveiral767@gmail.com>
2024-07-21 19:36:46 -0300

    dead letter exchange add

--- notificacao/src/main/java/br/com/mcoder/pedidos/notificacao/config/RabbitMQConfig.java
+++ notificacao/src/main/java/br/com/mcoder/pedidos/notificacao/config/RabbitMQConfig.java
@@ -15,6 +15,9 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RabbitMQConfig {
 
@@ -24,19 +27,43 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.queue.name}")
     private String queueName;
 
+    @Value("${rabbitmq.exchange.dlx.name}")
+    private String exchangeDlxName;
+
+    @Value("${rabbitmq.queue.dlq.name}")
+    private String queueDlqName;
+
     @Bean
     public FanoutExchange pedidosExchange(){
         return new FanoutExchange(exchangeName);
     }
 
     @Bean
+    public FanoutExchange pedidosDlxExchange(){
+        return new FanoutExchange(exchangeDlxName);
+    }
+
+    @Bean
     public Queue notificacaoQueue(){
-        return new Queue(queueName);
+
+        Map<String, Object> argumentos = new HashMap<>();
+        argumentos.put("x-dead-letter-exchange", exchangeDlxName);
+        return new Queue(queueName, true, false, false, argumentos);
+    }
+
+    @Bean
+    public Queue notificacaoDlqQueue(){
+        return new Queue(queueDlqName);
     }
 
     @Bean
     public Binding binding(){
         return BindingBuilder.bind(notificacaoQueue()).to(pedidosExchange());
+    }
+
+    @Bean
+    public Binding bindingDlq(){
+        return BindingBuilder.bind(notificacaoDlqQueue()).to(pedidosDlxExchange());
     }
 
     @Bean
